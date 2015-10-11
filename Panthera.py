47639e7edd85d172a6512854a81d25e3e77a01c7
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-11 15:55:09 +0800

    added the classes.

---
+++ Panthera.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+__author__ = 'raygomez'
+
+
+class Panthera(object):
+    def __init__(self):
+        self.name = ''
+        self.generation = ''
+        self.gender = ''
