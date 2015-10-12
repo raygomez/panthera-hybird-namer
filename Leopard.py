aa878dff6820aa21e00cf214dd9e8dbb86f3210a
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-12 12:25:44 +0800

    fixed test for Leopard.

--- Leopard.py
+++ Leopard.py
@@ -3,5 +3,14 @@ from Panthera import Panthera
 
 __author__ = 'raygomez'
 
+
 class Leopard(Panthera):
-    pass
+    def getMaleName(self):
+        return 'Leopard'
+
+    def getFemaleName(self):
+        return 'Leopardess'
+
+    def getOffspring(self, other):
+        if isinstance(other, Leopard):
+            return Leopard()
